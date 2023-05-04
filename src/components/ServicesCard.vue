3065efeab6c5b13f35f260aec30fed83674a9f1e
Markus <ModernFuzzBall2@hotmail.com>
2023-05-04 18:41:41 -0400

    Made the services cards interactive

---
+++ src/components/ServicesCard.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed, ref } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
+
+defineProps({
+    link: String,
+    image: String,
+    header: String,
+    description: String
+});
+
+const target = ref(null);
+
+const { elementX, elementY, isOutside, elementHeight, elementWidth } = useMouseInElement(target);
+
+const cardTransform = computed(() => {
+    const MAX_ROTATION = 6
+
+    const rX = (
+        MAX_ROTATION / 2 - (elementY.value / elementHeight.value) * MAX_ROTATION
+    ).toFixed(2) // handles x-axis
+
+    const rY = (
+        (elementX.value / elementWidth.value) * MAX_ROTATION - MAX_ROTATION / 2
+    ).toFixed(2) // handles y-axis
+
+    return isOutside.value ? '' : `perspective(${elementWidth.value}px) rotateX(${rX}deg) rotateY(${rY}deg)`
+});
+
+</script>
+
+<template>
+    <router-link 
+        ref="target" 
+        :to="link" 
+        :style="{
+            transform: cardTransform,
+            transition: 'transform 0.25s ease-out'
+        }" 
+        class="text-center bg-white bg-opacity-5 border border-white border-opacity-20 rounded-xl text-white text-opacity-80 hover:text-opacity-100"
+    >
+        <img :src="`/src/assets/${image}.svg`" alt="" class="aspect-square object-contain object-center p-8">
+        <div class="p-8 pt-0">
+            <h2 class="text-3xl font-black">{{ header }}</h2>
+            <p>{{ description }}</p>
+        </div>
+    </router-link>
+</template>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,23 +1,14 @@
+<script setup>
+import ServicesCard from '../components/ServicesCard.vue';
+</script>
 <template>
     <section class="pt-24">
         <div class="animate__animated animate__fadeIn container max-w-5xl mx-auto px-8">
-            <h1 class="text-6xl font-black text-white text-center">Services</h1>
+            <h1 class="text-6xl font-black text-white text-center mb-8">Services</h1>
             <div class="lg:grid grid-cols-3 gap-8">
-                <router-link to="/services/marketing" class="text-center hover:opacity-75 hover:scale-105 transition-all duration-300">
-                    <img src="../assets/billboard.svg" alt="" class="aspect-square object-contain object-center p-8">
-                    <h2 class="text-3xl font-black">Marketing</h2>
-                    <p>We can take care of your advertising using online and traditional methods.</p>
-                </router-link>
-                <router-link to="/services/web-development" class="text-center hover:opacity-75 hover:scale-105 transition-all duration-300">
-                    <img src="../assets/code.svg" alt="" class="aspect-square object-contain object-center p-8">
-                    <h2 class="text-3xl font-black">Web Development</h2>
-                    <p>We build custom static and dynamic websites using VueJS and Laravel.</p>
-                </router-link>
-                <router-link to="/services/branding" class="text-center hover:opacity-75 hover:scale-105 transition-all duration-300">
-                    <img src="../assets/megaphone.svg" alt="" class="aspect-square object-contain object-center p-8">
-                    <h2 class="text-3xl font-black text-center">Branding</h2>
-                    <p>We build brand images with color schemes logo designs and more.</p>
-                </router-link>
+                <ServicesCard link="/services/marketing" image="billboard" header="Marketing" description="We can take care of your advertising using online and traditional methods." />
+                <ServicesCard link="/services/web-development" image="code" header="Web Development" description="We build custom static and dynamic websites using VueJS and Laravel." />
+                <ServicesCard link="/services/branding" image="megaphone" header="Branding" description="We build brand images with color schemes logo designs and more." />
             </div>
         </div>
     </section>
