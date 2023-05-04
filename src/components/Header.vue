71e045a23c166e3ca9337e7348e89b10d072d531
Markus <ModernFuzzBall2@hotmail.com>
2023-05-04 12:49:09 -0400

    Updated the Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,10 @@
-<script>
+<script setup>
+import { ref } from 'vue';
+import Logo from '../icons/Logo.vue';
+import BarsIcon from '../icons/Bars.vue';
+import CloseIcon from '../icons/Close.vue';
 
-export default {
-    data() {
-        return {
-            mobileNav: false
-        }
-    }
-}
+let mobileNav = ref(false);
 
 </script>
 
@@ -15,10 +13,14 @@ export default {
         <div class="container max-w-6xl mx-auto">
             <nav class="py-2 mx-8 border-b border-white border-opacity-10 flex items-center justify-between">
                 <div class="w-32 flex sm:hidden">
-                    <button @click="mobileNav = true"><img src="../assets/bars.svg" class="w-6 h-6"></button>
+                    <button @click="mobileNav = true">
+                        <BarsIcon class="w-6 h-6 text-white" />
+                    </button>
                 </div>
                 <div class="flex items-center">
-                    <router-link to="/" class="hover:opacity-50 transition-all duration-150 sm:mr-8"><img src="../assets/logo.svg" class="h-8 w-8"></router-link>
+                    <router-link to="/" class="hover:opacity-50 transition-all duration-150 sm:mr-8">
+                        <Logo class="w-8 h-8" />
+                    </router-link>
                     <ul class="hidden sm:flex items-center">
                         <li class="mx-2"><router-link :to="{ name: 'Home' }" class="text-sm hover:opacity-50 transition-all duration-150">{{ $t("nav.home") }}</router-link></li>
                         <li class="mx-2"><router-link :to="{ name: 'Services' }" class="text-sm hover:opacity-50 transition-all duration-150">{{ $t("nav.services") }}</router-link></li>
@@ -31,8 +33,10 @@ export default {
             </nav>
         </div>
         <nav v-if="mobileNav" class="fixed z-50 top-0 left-0 w-full h-screen bg-black bg-opacity-75 backdrop-blur-md text-center pt-24">
-            <button @click="mobileNav = false" class="absolute top-3 left-8"><img src="../assets/close.svg" class="h-6 w-6"></button>
-            <div class="animate__animated animate__zoomIn">
+            <button @click="mobileNav = false" class="absolute top-3 left-8">
+                <CloseIcon class="h-6 w-6" />
+            </button>
+            <div class="animate__animated animate__zoomIn animate__faster">
                 <router-link @click="mobileNav = false" to="/" class="block text-6xl font-bold mb-2">{{ $t("nav.home") }}</router-link>
                 <router-link @click="mobileNav = false" to="/services" class="block text-6xl font-bold mb-2">{{ $t("nav.services") }}</router-link>
                 <router-link @click="mobileNav = false" to="/portfolio" class="block text-6xl font-bold mb-2">{{ $t("nav.portfolio") }}</router-link>
