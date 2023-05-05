14298efb32f4b1cba6a23f1a971bb908568dcc56
Markus <ModernFuzzBall2@hotmail.com>
2023-05-05 14:17:06 -0400

    Renamed Services Card to Perspective Card

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,27 +1,32 @@
-<script>
+<script setup>
+import PerspectiveCard from '../components/PerspectiveCard.vue';
+
 
 </script>
 
 <template>
     <section class="pt-24">
         <div class="animate__animated animate__fadeIn container max-w-5xl mx-auto px-8">
-            <h1 class="text-6xl font-black text-white text-center">Projects</h1>
+            <h1 class="text-6xl font-black text-white text-center mb-8">Projects</h1>
             <div class="lg:grid grid-cols-3 gap-8">
-                <router-link to="/projects/spigot-plugins" class="text-center hover:brightness-75 hover:scale-105 transition-all duration-300">
-                    <img src="../assets/bucket.svg" alt="" class="aspect-square object-contain object-center p-16 pb-0">
-                    <h2 class="text-3xl font-black">Spigot Plugins</h2>
-                    <p>Check out our Spigot Plugins</p>
-                </router-link>
-                <router-link to="/projects/web-apps" class="text-center hover:brightness-75 hover:scale-105 transition-all duration-300">
-                    <img src="../assets/browser.svg" alt="" class="aspect-square object-contain object-center p-16 pb-0">
-                    <h2 class="text-3xl font-black">Web Apps</h2>
-                    <p>Check out our Web Apps</p>
-                </router-link>
-                <router-link to="/projects/web-games" class="text-center hover:brightness-75 hover:scale-105 transition-all duration-300">
-                    <img src="../assets/gamepad.svg" alt="" class="aspect-square object-contain object-center p-16 pb-0">
-                    <h2 class="text-3xl font-black">Web Games</h2>
-                    <p>Check out our Web Games</p>
-                </router-link>
+                <PerspectiveCard link="/projects/spigot-plugins">
+                    <div class="p-8">
+                        <h2 class="text-3xl font-black">Spigot Plugins</h2>
+                        <p>Check out our Spigot Plugins</p>
+                    </div>
+                </PerspectiveCard>
+                <PerspectiveCard link="/projects/web-apps">
+                    <div class="p-8">
+                        <h2 class="text-3xl font-black">Web Apps</h2>
+                        <p>Check out our Web Apps</p>
+                    </div>
+                </PerspectiveCard>
+                <PerspectiveCard link="/projects/web-games">
+                    <div class="p-8">
+                        <h2 class="text-3xl font-black">Web Games</h2>
+                        <p>Check out our Web Games</p>
+                    </div>
+                </PerspectiveCard>
             </div>
         </div>
     </section>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,32 +1,32 @@
 <script setup>
-import ServicesCard from '../components/ServicesCard.vue';
+import PerspectiveCard from '../components/PerspectiveCard.vue';
 </script>
 <template>
     <section class="pt-24">
         <div class="animate__animated animate__fadeIn container max-w-5xl mx-auto px-8">
             <h1 class="text-6xl font-black text-white text-center mb-8">Services</h1>
             <div class="lg:grid grid-cols-3 gap-8">
-                <ServicesCard link="/services/marketing">
+                <PerspectiveCard link="/services/marketing">
                     <img src="../assets/billboard.svg" alt="" class="aspect-square object-contain object-center p-8">
                     <div class="p-8 pt-0">
                         <h2 class="text-3xl font-black">Marketing</h2>
                         <p>We can take care of your advertising using online and traditional methods.</p>
                     </div>
-                </ServicesCard>
-                <ServicesCard link="/services/web-development">
+                </PerspectiveCard>
+                <PerspectiveCard link="/services/web-development">
                     <img src="../assets/code.svg" alt="" class="aspect-square object-contain object-center p-8">
                     <div class="p-8 pt-0">
                         <h2 class="text-3xl font-black">Web Development</h2>
                         <p>We build custom static and dynamic websites using VueJS and Laravel.</p>
                     </div>
-                </ServicesCard>
-                <ServicesCard link="/services/branding">
+                </PerspectiveCard>
+                <PerspectiveCard link="/services/branding">
                     <img src="../assets/megaphone.svg" alt="" class="aspect-square object-contain object-center p-8">
                     <div class="p-8 pt-0">
                         <h2 class="text-3xl font-black">Branding</h2>
                         <p>We build brand images with color schemes logo designs and more.</p>
                     </div>
-                </ServicesCard>
+                </PerspectiveCard>
             </div>
         </div>
     </section>
