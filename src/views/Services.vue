667d8e03f0dd4d8a0aab62b63fb4107b74aea97c
Markus <ModernFuzzBall2@hotmail.com>
2023-05-05 00:05:29 -0400

    Update Services.vue

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -7,21 +7,21 @@ import ServicesCard from '../components/ServicesCard.vue';
             <h1 class="text-6xl font-black text-white text-center mb-8">Services</h1>
             <div class="lg:grid grid-cols-3 gap-8">
                 <ServicesCard link="/services/marketing">
-                    <img :src="`../src/assets/billboard.svg`" alt="" class="aspect-square object-contain object-center p-8">
+                    <img src="../src/assets/billboard.svg" alt="" class="aspect-square object-contain object-center p-8">
                     <div class="p-8 pt-0">
                         <h2 class="text-3xl font-black">Marketing</h2>
                         <p>We can take care of your advertising using online and traditional methods.</p>
                     </div>
                 </ServicesCard>
                 <ServicesCard link="/services/web-development">
-                    <img :src="`../src/assets/code.svg`" alt="" class="aspect-square object-contain object-center p-8">
+                    <img src="../src/assets/code.svg" alt="" class="aspect-square object-contain object-center p-8">
                     <div class="p-8 pt-0">
                         <h2 class="text-3xl font-black">Web Development</h2>
                         <p>We build custom static and dynamic websites using VueJS and Laravel.</p>
                     </div>
                 </ServicesCard>
                 <ServicesCard link="/services/branding">
-                    <img :src="`../src/assets/megaphone.svg`" alt="" class="aspect-square object-contain object-center p-8">
+                    <img src="../src/assets/megaphone.svg" alt="" class="aspect-square object-contain object-center p-8">
                     <div class="p-8 pt-0">
                         <h2 class="text-3xl font-black">Branding</h2>
                         <p>We build brand images with color schemes logo designs and more.</p>
