2a119786489516fe325ed637a396bc0a504a9113
Markus <ModernFuzzBall2@hotmail.com>
2023-05-07 19:40:13 -0400

    Added translations and a few optimizations

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,11 @@ import { ref } from 'vue';
 import Logo from '../icons/Logo.vue';
 import BarsIcon from '../icons/Bars.vue';
 import CloseIcon from '../icons/Close.vue';
+import { useWindowSize } from '@vueuse/core';
+const { width } = useWindowSize();
+
+let smallWidth = true;
+if (width.value > 520) smallWidth = false;
 
 let mobileNav = ref(false);
 
@@ -12,7 +17,7 @@ let mobileNav = ref(false);
     <header class="animate__animated animate__fadeIn fixed z-40 top-0 left-0 w-full bg-black bg-opacity-50 backdrop-blur-md">
         <div class="container max-w-6xl mx-auto">
             <nav class="py-2 mx-8 border-b border-white border-opacity-10 flex items-center justify-between">
-                <div class="w-32 flex sm:hidden">
+                <div class="w-48 flex sm:hidden">
                     <button @click="mobileNav = true">
                         <BarsIcon class="w-6 h-6 text-white" />
                     </button>
@@ -29,20 +34,22 @@ let mobileNav = ref(false);
                         <li class="mx-2"><router-link :to="{ name: 'About' }" class="text-sm hover:opacity-50 transition-all duration-150">{{ $t("nav.about") }}</router-link></li>
                     </ul>
                 </div>
-                <router-link :to="{ name: 'Contact'}" class="border-2 border-white px-3 py-1 font-medium text-sm rounded-full text-white hover:bg-white hover:text-black transition-all duration-150">{{ $t("nav.getintouch") }}</router-link>
+                <div class="w-48 text-right">
+                    <router-link :to="{ name: 'Contact'}" class="border-2 border-white px-3 py-1 font-medium text-sm rounded-full text-white hover:bg-white hover:text-black transition-all duration-150 uppercase">{{ smallWidth ? $t("nav.contact") : $t("nav.getintouch") }}</router-link>
+                </div>
             </nav>
         </div>
-        <nav v-if="mobileNav" class="fixed z-50 top-0 left-0 w-full h-screen bg-black bg-opacity-75 backdrop-blur-md text-center pt-24">
-            <button @click="mobileNav = false" class="absolute top-3 left-8">
-                <CloseIcon class="h-6 w-6" />
-            </button>
-            <div class="animate__animated animate__zoomIn animate__faster">
-                <router-link @click="mobileNav = false" to="/" class="block text-6xl font-bold mb-2">{{ $t("nav.home") }}</router-link>
-                <router-link @click="mobileNav = false" to="/services" class="block text-6xl font-bold mb-2">{{ $t("nav.services") }}</router-link>
-                <router-link @click="mobileNav = false" to="/portfolio" class="block text-6xl font-bold mb-2">{{ $t("nav.portfolio") }}</router-link>
-                <router-link @click="mobileNav = false" to="/projects" class="block text-6xl font-bold mb-2">{{ $t("nav.projects") }}</router-link>
-                <router-link @click="mobileNav = false" to="/about" class="block text-6xl font-bold mb-2">{{ $t("nav.about") }}</router-link>
-            </div>
-        </nav>
     </header>
+    <nav v-if="mobileNav" class="fixed z-50 top-0 left-0 w-full h-screen bg-black bg-opacity-75 backdrop-blur-md text-center pt-24">
+        <button @click="mobileNav = false" class="absolute top-3 left-8">
+            <CloseIcon class="h-6 w-6" />
+        </button>
+        <div class="animate__animated animate__zoomIn animate__faster">
+            <router-link @click="mobileNav = false" to="/" class="block text-6xl font-bold mb-2">{{ $t("nav.home") }}</router-link>
+            <router-link @click="mobileNav = false" to="/services" class="block text-6xl font-bold mb-2">{{ $t("nav.services") }}</router-link>
+            <router-link @click="mobileNav = false" to="/portfolio" class="block text-6xl font-bold mb-2">{{ $t("nav.portfolio") }}</router-link>
+            <router-link @click="mobileNav = false" to="/projects" class="block text-6xl font-bold mb-2">{{ $t("nav.projects") }}</router-link>
+            <router-link @click="mobileNav = false" to="/about" class="block text-6xl font-bold mb-2">{{ $t("nav.about") }}</router-link>
+        </div>
+    </nav>
 </template>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -11,24 +11,24 @@ let page = "";
 <template>
     <section class="pt-24">
         <div class="animate__animated animate__fadeIn container max-w-5xl mx-auto px-8">
-            <h1 class="text-6xl font-black text-white text-center mb-8">Projects</h1>
+            <h1 class="text-6xl font-black text-white text-center mb-8">{{ $t("projects.projects") }}</h1>
             <div class="lg:grid grid-cols-3 gap-8">
                 <PerspectiveCard @click="page = 'spigot-plugins'" link="projects">
                     <div class="p-8">
-                        <h2 class="text-3xl font-black">Spigot Plugins</h2>
-                        <p>Check out our Spigot Plugins</p>
+                        <h2 class="text-3xl font-black">{{ $t("projects.spigotplugins") }}</h2>
+                        <p>{{ $t("projects.checkout") + $t("projects.spigotplugins") }}</p>
                     </div>
                 </PerspectiveCard>
                 <PerspectiveCard link="/projects/web-apps">
                     <div class="p-8">
-                        <h2 class="text-3xl font-black">Web Apps</h2>
-                        <p>Check out our Web Apps</p>
+                        <h2 class="text-3xl font-black">{{ $t("projects.webapps") }}</h2>
+                        <p>{{ $t("projects.checkout") + $t("projects.webapps") }}</p>
                     </div>
                 </PerspectiveCard>
                 <PerspectiveCard link="/projects/web-games">
                     <div class="p-8">
-                        <h2 class="text-3xl font-black">Web Games</h2>
-                        <p>Check out our Web Games</p>
+                        <h2 class="text-3xl font-black">{{ $t("projects.webgames") }}</h2>
+                        <p>{{ $t("projects.checkout") + $t("projects.webgames") }}</p>
                     </div>
                 </PerspectiveCard>
             </div>
