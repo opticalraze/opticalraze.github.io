7e0b99df95d1103a4225d2a9fd5b4279adc0a268
Markus <ModernFuzzBall2@hotmail.com>
2023-05-09 03:18:39 -0400

    Added Carry On Packing Travel Agency to the portfolio page

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,8 +1,16 @@
 <template>
     <section class="pt-24">
         <div class="animate__animated animate__fadeIn container max-w-5xl mx-auto px-8">
-            <h1 class="text-6xl font-black text-white text-center">Portfolio</h1>
+            <h1 class="text-6xl font-black text-white text-center mb-16">Portfolio</h1>
             
+            <div class="grid grid-cols-3 gap-4">
+                <a href="/carry-on-packing-travel-agency">
+                    <img src="../assets/carry-on-packing-travel-agency.jpg" alt="" class="mb-4">
+                    <h3 class="text-2xl font-black">Carry On Packing.</h3>
+                    <h4 class="font-thin">Travel Agency</h4>
+                </a>
+            </div>
+
         </div>
     </section>
 </template>
