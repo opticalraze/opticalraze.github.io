60b728da1099c6e204a6991a2938d4692421bef6
Markus <ModernFuzzBall2@hotmail.com>
2023-05-09 16:44:17 -0400

    Update Portfolio.vue

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -5,7 +5,7 @@
             
             <div class="grid grid-cols-3 gap-4">
                 <a href="/carry-on-packing-travel-agency">
-                    <img src="../assets/carry-on-packing-travel-agency.jpg" alt="" class="mb-4">
+                    <img src="/carry-on-packing-travel-agency/ogimage.jpg" alt="" class="mb-4">
                     <h3 class="text-2xl font-black">Carry On Packing.</h3>
                     <h4 class="font-thin">Travel Agency</h4>
                 </a>
