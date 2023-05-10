a709413e9c8f31645b9abf1a154a70ac7b36b002
Markus <ModernFuzzBall2@hotmail.com>
2023-05-09 21:38:39 -0400

    Update Portfolio.vue

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -4,10 +4,15 @@
             <h1 class="text-6xl font-black text-white text-center mb-16">Portfolio</h1>
             
             <div class="grid grid-cols-3 gap-4">
-                <a href="/carry-on-packing-travel-agency">
+                <a href="/carry-on-packing-travel-agency/">
                     <img src="https://opticalraze.com/carry-on-packing-travel-agency/ogimage.jpg" alt="" class="mb-4">
                     <h3 class="text-2xl font-black">Carry On Packing.</h3>
                     <h4 class="font-thin">Travel Agency</h4>
+                </a>
+                <a href="/petcare/">
+                    <img src="https://opticalraze.com/petcare/ogimage.jpg" alt="" class="mb-4">
+                    <h3 class="text-2xl font-black">PetCare</h3>
+                    <h4 class="font-thin">Pet Services</h4>
                 </a>
             </div>
 
