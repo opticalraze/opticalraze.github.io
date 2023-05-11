1e86eff6c3604ac679b89b470c21f369e123b59d
Markus <ModernFuzzBall2@hotmail.com>
2023-05-11 13:20:58 -0400

    Added social links

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,4 +13,28 @@
             <p class="text-xl text-center mt-8">{{ $t("about.tagline3") }}</p>
         </div>
     </section>
+    <section>
+        <div class="grid grid-cols-5 gap-4 max-w-2xl mx-auto">
+            <a href="https://www.instagram.com/opticalraze/" target="_blank" class="flex flex-col justify-center items-center hover:brightness-50 transition-all duration-300">
+                <img src="../assets/instagram.svg" alt="Instagram" class="h-12 w-12 mb-2">
+                <h6 class="text-xl font-black">Instagram</h6>
+            </a>
+            <a href="https://twitter.com/opticalraze" target="_blank" class="flex flex-col justify-center items-center hover:brightness-50 transition-all duration-300">
+                <img src="../assets/twitter.svg" alt="Twitter" class="h-12 w-12 mb-2">
+                <h6 class="text-xl font-black">Twitter</h6>
+            </a>
+            <a href="https://www.facebook.com/opticalraze/" target="_blank" class="flex flex-col justify-center items-center hover:brightness-50 transition-all duration-300">
+                <img src="../assets/facebook.svg" alt="Facebook" class="h-12 w-12 mb-2">
+                <h6 class="text-xl font-black">Facebook</h6>
+            </a>
+            <a href="https://www.youtube.com/@opticalraze" target="_blank" class="flex flex-col justify-center items-center hover:brightness-50 transition-all duration-300">
+                <img src="../assets/youtube.svg" alt="YouTube" class="h-12 w-12 mb-2">
+                <h6 class="text-xl font-black">YouTube</h6>
+            </a>
+            <a href="https://github.com/opticalraze" target="_blank" class="flex flex-col justify-center items-center hover:brightness-50 transition-all duration-300">
+                <img src="../assets/github.svg" alt="GitHub" class="h-12 w-12 mb-2">
+                <h6 class="text-xl font-black">GitHub</h6>
+            </a>
+        </div>
+    </section>
 </template>
