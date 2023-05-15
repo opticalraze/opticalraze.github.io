40ac08d6838d1fd264117459c0229f08e8de0fe2
Markus <ModernFuzzBall2@hotmail.com>
2023-05-14 20:35:50 -0400

    Added the plaque to the about page

---
+++ src/components/Plaque.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="plaque">
+        <img src="/logo.svg" alt="Optical Raze">
+        <h6>Developed by <b>Optical Raze</b></h6>
+    </div>
+</template>
+
+<style scoped>
+.plaque {
+  @apply block w-24 bg-black p-4 text-white text-center border border-[#8A6E2F] rounded-lg hover:brightness-50 transition-all duration-300;
+  background: radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%), radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%);
+}
+.plaque h6 {
+  font-size: .5rem;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,7 @@
+<script setup>
+import Plaque from '../components/Plaque.vue';
+</script>
+
 <template>
     <section class="px-8 h-screen w-full flex items-center justify-center">
         <div class="animate__animated animate__fadeIn text-center sm:text-left">
@@ -11,6 +15,16 @@
             <h1 class="text-6xl lg:text-8xl font-black text-black text-outline-white">{{ $t("about.ourmission") }}</h1>
             <h2 class="text-4xl lg:text-6xl font-black text-white">{{ $t("about.simplyput") }}</h2>
             <p class="text-xl text-center mt-8">{{ $t("about.tagline3") }}</p>
+        </div>
+    </section>
+    <section class="px-8 h-screen w-full flex items-center justify-center">
+        <div class="text-center sm:text-left">
+            <h1 class="text-6xl lg:text-8xl font-black text-black text-outline-white">{{ $t("about.mark") }}</h1>
+            <h2 class="text-4xl lg:text-6xl font-black text-white">{{ $t("about.proud") }}</h2>
+            <p class="text-xl text-center my-8">{{ $t("about.plaque") }}</p>
+
+            <Plaque class="mx-auto" />
+
         </div>
     </section>
     <section>
