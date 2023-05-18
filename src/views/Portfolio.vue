8361d2fdc596b187e541471cf58be0321a97f02c
Markus <ModernFuzzBall2@hotmail.com>
2023-05-18 02:03:35 -0400

    Added Nolan's Portfolio to the Portfolio Page

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -3,16 +3,21 @@
         <div class="animate__animated animate__fadeIn container max-w-5xl mx-auto px-8">
             <h1 class="text-6xl font-black text-white text-center mb-16">Portfolio</h1>
             
-            <div class="grid grid-cols-3 gap-4">
-                <a href="/carry-on-packing-travel-agency/">
-                    <img src="https://opticalraze.com/carry-on-packing-travel-agency/ogimage.jpg" alt="" class="mb-4">
+            <div class="grid grid-cols-3 gap-8">
+                <a href="https://opticalraze.com/carry-on-packing-travel-agency/" target="_blank">
+                    <img src="../assets/portfolio/carryonpacking.webp" alt="" class="mb-4">
                     <h3 class="text-2xl font-black">Carry On Packing.</h3>
                     <h4 class="font-thin">Travel Agency</h4>
                 </a>
-                <a href="/petcare/">
-                    <img src="https://opticalraze.com/petcare/ogimage.jpg" alt="" class="mb-4">
+                <a href="https://opticalraze.com/petcare/" target="_blank">
+                    <img src="../assets/portfolio/petcare.webp" alt="" class="mb-4">
                     <h3 class="text-2xl font-black">PetCare</h3>
                     <h4 class="font-thin">Pet Services</h4>
+                </a>
+                <a href="https://nolanpilon.github.io" target="_blank">
+                    <img src="../assets/portfolio/nolanpilon.webp" alt="nolanpilon.github.io" class="mb-4">
+                    <h3 class="text-2xl font-black">Nolan's Portfolio</h3>
+                    <h4 class="font-thin">Game Programmer</h4>
                 </a>
             </div>
 
